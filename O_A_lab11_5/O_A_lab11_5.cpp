60f595ebc0f74cd1644bb3237f9758117485f8b8
tagelharpa <76700337+tagelharpa@users.noreply.github.com>
2023-06-12 22:02:51 +0300

    lab11_5

---
+++ O_A_lab11_5/O_A_lab11_5.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include "my_vector_11_5.h"
+#include "time_class.h"
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	std::cout << "Введите количество элементов: ";
+	int n;
+	std::cin >> n;
+
+	MyVector<Time> vec(n);
+	vec.print("Начальный вектор: ");
+
+	vec.add_max_to_begin();
+	vec.print("Максимальный объект добавлен в начало: ");
+
+	vec.erase_min();
+	vec.print("Удаляется минимальный объект: ");
+
+	vec.inc_average();
+	vec.print("Cреднее арифметическое для объектов: ");
+
+	std::cin.ignore();
+	std::cin.get();
+	return 0;
+}
